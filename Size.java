47f058e0460e3f893d7d52aec91770746e5b9a8f
gabrielLimaaaa <200290128@estudantes.ips.pt>
2021-03-04 11:21:57 -0300

    1 class.

---
+++ Size.java
@@ -0,0 +1,42 @@
+public enum Size {
+    
+    SMALL(32, 36, "pequeno", 'S'),
+    MEDIUM(37, 44, "medio", 'M'),
+    LARGE(45, 52, "grande", 'G');
+    
+    private final String description;
+    private final int minValue;
+    private final int maxValue;
+    private final char code;
+
+    private Size(int minValue, int maxValue, String description, char code) {
+
+        this.description = description;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+        this.code = code;
+        
+    }
+    
+    @override
+
+    public String toString() {
+        return this.description;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public int getMinValue() {
+        return minValue;
+    }
+    
+    public int getMaxValue() {
+        return maxValue;
+    }
+    
+    public char getCode() {
+        return code;
+    }
+}
